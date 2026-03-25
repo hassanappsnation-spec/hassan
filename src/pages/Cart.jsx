da6a82d7f709583a07bac3898d51570913cc8efb
Hassan-raza <hassanappsnation@gmail.com>
2026-03-25 20:21:46 +0500

    again

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const cartItems = useSelector((state) => state.cart.cartItem);
 
-export default Cart
+  return (
+    <div>
+      <h2>Cart Items</h2>
+
+      {cartItems.length === 0 ? (
+        <p>Cart is empty</p>
+      ) : (
+        cartItems.map((item) => (
+          <div key={item.id}>
+            <img src={item.image} alt="" />
+            <h3>{item.name}</h3>
+            <p>Price: {item.price}</p>
+            <p>Quantity: {item.quantity}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Cart;
